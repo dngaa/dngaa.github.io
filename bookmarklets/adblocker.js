5a23d22ed96890f5a1f9d6824dddd35905a06651
Jonathon50 <93009498+Jonathon50@users.noreply.github.com>
2022-03-09 15:24:06 -0600

    Create adblocker.js

---
+++ bookmarklets/adblocker.js
@@ -0,0 +1,1 @@
+javascript:void(F=document.getElementsByTagName('iframe'));for(f=0;f<F.length;f++)void(F[f].src='about:');void(F=document.getElementsByTagName('img'));for(f=0;f<F.length;f++)if(F[f].width=='468')void(F[f].src='about:');
