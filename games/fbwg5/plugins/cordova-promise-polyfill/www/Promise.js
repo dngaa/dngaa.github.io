bd22e4c2684caf3d49023b7e6ba21667f326c884
dngaa <christian.burek_2023@navarroisd.us>
2022-04-05 19:07:33 -0500

    yuh

--- games/fbwg5/plugins/cordova-promise-polyfill/www/Promise.js
+++
@@ -1,6 +0,0 @@
-﻿cordova.define("cordova-promise-polyfill.Promise", function(require, exports, module) {
-if (typeof window !== 'undefined' && !window.Promise) {
-  window.Promise = require('./promise.min')
-}
-
-});
