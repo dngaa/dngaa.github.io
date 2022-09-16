5aae888d7e1a3252d21b6e4251a57d8d70e4b333
dngaa <98403397+dngaa@users.noreply.github.com>
2022-09-16 18:06:03 -0500

    Delete bookmarklets directory

--- bookmarklets/adblocker.js
+++
@@ -1,1 +0,0 @@
-javascript:void(F=document.getElementsByTagName('iframe'));for(f=0;f<F.length;f++)void(F[f].src='about:');void(F=document.getElementsByTagName('img'));for(f=0;f<F.length;f++)if(F[f].width=='468')void(F[f].src='about:');
