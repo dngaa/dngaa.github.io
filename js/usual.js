b0d8a98f7a9f85cf718017c7a079ff07dd2728a3
om3ga6400 <JackGA6400@gmail.com>
2025-04-29 18:25:03 +0000

    a lot of small changes

--- js/usual.js
+++ js/usual.js
@@ -1,116 +1,31 @@
-// Configuration object for particles.js
-const particlesConfig = {
-  "particles": {
-      "number": {
-          "value": 62,
-          "density": {
-              "enable": true,
-              "value_area": 800
-          }
-      },
-      "color": {
-          "value": "#ffffff"
-      },
-      "shape": {
-          "type": "circle",
-          "stroke": {
-              "width": 0,
-              "color": "#000000"
+(async () => {
+  await loadFull(tsParticles);
+  
+  await tsParticles.load({
+    id: "tsparticles",
+    options: {
+      particles: {
+        number: {
+          value: 100,
+        },
+        size: {
+          value: 1,
+        },
+        links: {
+          enable: true,
+          distance: 115,
+          opacity: 0.25,
+        },
+        move: {
+          enable: true,
+          speed: 1.75,
+          outModes: {
+            default: "bounce",
           },
-          "polygon": {
-              "nb_sides": 5
-          },
-          "image": {
-              "src": "img/github.svg",
-              "width": 100,
-              "height": 100
-          }
+        },
       },
-      "opacity": {
-          "value": 0.5,
-          "random": false,
-          "anim": {
-              "enable": true,
-              "speed": 0.5,
-              "opacity_min": 0.015984015984015984,
-              "sync": false
-          }
-      },
-      "size": {
-          "value": 3,
-          "random": true,
-          "anim": {
-              "enable": false,
-              "speed": 40,
-              "size_min": 0.1,
-              "sync": false
-          }
-      },
-      "line_linked": {
-          "enable": true,
-          "distance": 150,
-          "color": "#ffffff",
-          "opacity": 0.4,
-          "width": 1
-      },
-      "move": {
-          "enable": true,
-          "speed": 1,
-          "direction": "none",
-          "random": false,
-          "straight": false,
-          "out_mode": "out",
-          "bounce": false,
-          "attract": {
-              "enable": false,
-              "rotateX": 600,
-              "rotateY": 1200
-          }
-      }
-  },
-  "interactivity": {
-    "detect_on": "canvas",
-    "events": {
-      "onhover": {
-        "enable": false, // Disable hover interactions
-        "mode": "grab"
-      },
-      "onclick": {
-        "enable": false, // Disable click interactions
-        "mode": "push"
-      },
-      "resize": true
     },
-    "modes": {
-      "grab": {
-        "distance": 400,
-        "line_linked": {
-          "opacity": 1
-        }
-      },
-      "bubble": {
-        "distance": 400,
-        "size": 40,
-        "duration": 2,
-        "opacity": 8,
-        "speed": 3
-      },
-      "repulse": {
-        "distance": 200,
-        "duration": 0.4
-      },
-      "push": {
-        "particles_nb": 4
-      },
-      "remove": {
-        "particles_nb": 2
-      }
-    }
-  },
-  "retina_detect": true
-};
+  });
+})();
 
-// Initialize particles.js with the configuration object
-particlesJS('particles-js', particlesConfig);
-
-console.log('callback - particles.js config loaded');
+console.log('callback - tsparticles.js config loaded');
